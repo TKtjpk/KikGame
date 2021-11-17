e37a5d0749bb264bec98efb518e4d37ea291b2b7
Tomasz <80661109+TKtjpk@users.noreply.github.com>
2021-11-17 13:11:15 +0100

    Initial Commit

---
+++ KikGame/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  KikGame
+//
+//  Created by MacBook Pro (Tomasz) on 17/11/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
